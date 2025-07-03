60a5e1270f49100c7c6f4ebc19a8e1bcaa8126ed
Ben Hollar <benh524@gmail.com>
2025-07-03 15:49:23 -0400

    Fixing dates for RSS in system-timezone-independent way; link to RSS feed

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -2,6 +2,7 @@ import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
 import { loadRenderers } from "astro:container";
 import { getCollection, render } from "astro:content";
+import type { AstroGlobal } from "astro";
 import rss from "@astrojs/rss";
 
 import { parse as htmlParser } from "node-html-parser";
@@ -9,7 +10,7 @@ import { parse as htmlParser } from "node-html-parser";
 // Adapted from https://blog.damato.design/posts/astro-rss-mdx/ for modern MDX support
 //  I had to shoehorn in support for Astro's dynamic image optimization, replacing the "src" attribute of all images in
 //  each post.
-export async function GET(context) {
+export async function GET(context: AstroGlobal) {
     const renderers = await loadRenderers([getMDXRenderer()]);
     const container = await AstroContainer.create({ renderers });
     const posts = await getCollection('blog');
@@ -21,12 +22,12 @@ export async function GET(context) {
         const html = htmlParser.parse(content);
         const images = html.querySelectorAll("img");
         for (const img of images) {
-            const src = decodeURIComponent(img.getAttribute("src")).replace("/_image?href=", "").split("?")[0];
+            const src = decodeURIComponent(img.getAttribute("src")!).replace("/_image?href=", "").split("?")[0];
             img.setAttribute("src", src);
         }
         content = html.toString();
 
-        post.data.date.setTime(post.data.date.getTime() + post.data.date.getTimezoneOffset() * 60 * 1000);
+        post.data.date.setTime(post.data.date.getTime() + -getTimezoneOffset("America/New_York") * 60 * 1000);
         items.push({
             title: post.data.title,
             pubDate: post.data.date,
@@ -39,7 +40,13 @@ export async function GET(context) {
     return rss({
         title: 'Ben Hollar - Blog',
         description: 'The personal blog of Ben Hollar, a full-stack developer and hobbyist nature photographer.',
-        site: context.site,
+        site: context.site ?? "https://www.benhollar.com",
         items,
     });
+}
+
+function getTimezoneOffset(timeZone: string) {
+  const str = new Date().toLocaleString('en', {timeZone, timeZoneName: 'longOffset'});
+  const [_, hours, minutes] = str.match(/([+-]\d+):(\d+)$/) || [, '+00', '00'];
+  return Number(hours) * 60 + (Number(hours) > 0 ? +minutes : -minutes);
 }
