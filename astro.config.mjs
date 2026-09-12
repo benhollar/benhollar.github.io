3e4073890068d4b763365fffce43eae0c1ca7d6e
Ben Hollar <benh524@gmail.com>
2026-09-11 20:38:58 -0400

    Integrating React; updating all other packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 // @ts-check
+import { satteri } from "@astrojs/markdown-satteri";
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
-import { remarkReadingTime } from './src/plugins/remark-reading-timew.mjs';
+
+import react from '@astrojs/react';
+import { mdastReadingTimePlugin } from './src/plugins/mdast-reading-time';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://benhollar.com",
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   markdown: {
     shikiConfig: {
       theme: "slack-ochin"
     },
-    remarkPlugins: [remarkReadingTime]
+    processor: satteri({
+      mdastPlugins: [mdastReadingTimePlugin]
+    })
   }
 });
