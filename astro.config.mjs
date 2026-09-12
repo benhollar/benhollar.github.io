1941941ff5b078fcf360c53e93c9ee1b7200afc1
Ben Hollar <benh524@gmail.com>
2026-09-11 21:46:46 -0400

    Remove React / MUI

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,12 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
-import react from '@astrojs/react';
 import { mdastReadingTimePlugin } from './src/plugins/mdast-reading-time';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://benhollar.com",
-  integrations: [mdx(), react()],
+  integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: "slack-ochin"
